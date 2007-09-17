297a1ea542315e741a88c0dfc2c7562a624b2fe0
Christophe Rhodes <csr21@cantab.net>
2007-09-17 14:33:54 +0000

    Improve simple insert/query test case
    git-svn-id: svn+ssh://rvw/isms/group/omras2/src/svn/audioDB/trunk@60 7f337319-b335-0410-8187-a933a6394831

--- tests/0003/run-test.sh
+++ tests/0003/run-test.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+trap "exit 1" ERR
+
 if [ -f testdb ]; then rm -f testdb; fi
 
 ${AUDIODB} -d testdb -N
@@ -9,10 +11,17 @@ printf "\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\xf0\x3f" > testfeature
 
 ${AUDIODB} -d testdb -I -f testfeature
 
-${AUDIODB} -d testdb -Q point -f /tmp/foo > query-output
+${AUDIODB} -d testdb -Q point -f testfeature > query-output
 
 echo testfeature 1 0 0 > test-query-output
 
 cmp query-output test-query-output
+
+# failure cases
+${AUDIODB} -d testdb -I && exit 1
+${AUDIODB} -d testdb -f testfeature && exit 1
+${AUDIODB} -I -f testfeature && exit 1
+${AUDIODB} -d testdb -Q notpoint -f testfeature && exit 1
+${AUDIODB} -Q point -f testfeature && exit 1
 
 exit 104
