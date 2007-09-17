8f7bacedaad7704502dcc2c40bcaba9bc51cf553
Christophe Rhodes <csr21@cantab.net>
2007-09-17 12:12:30 +0000

    Make run-tests work with more than one test directory
    git-svn-id: svn+ssh://rvw/isms/group/omras2/src/svn/audioDB/trunk@57 7f337319-b335-0410-8187-a933a6394831

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -13,7 +13,8 @@ fi
 for file in [0-9][0-9][0-9][0-9]*; do
   if [ -d ${file} ]; then
     if [ -f ${file}/run-test.sh ]; then
-      cd ${file} && sh ./run-test.sh > test.out 2> test.err
+      echo Running test ${file}
+      (cd ${file} && sh ./run-test.sh > test.out 2> test.err)
       EXIT_STATUS=$?
       if [ ${EXIT_STATUS} -ne 104 ]; then
         echo Test ${file} failed: exit status ${EXIT_STATUS}
