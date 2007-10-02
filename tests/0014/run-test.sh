a8efd418bef8c9754783ba91c38de5240d18b55c
Christophe Rhodes <csr21@cantab.net>
2007-10-02 15:35:10 +0000

    Add WS test for point query search (exactly like 0004).
    Fails because result list lengt is unconditionally set to pointNN 
    (rather than allowing for fewer matches.)
    
    
    git-svn-id: svn+ssh://rvw/isms/group/omras2/src/svn/audioDB/trunk@104 7f337319-b335-0410-8187-a933a6394831

---
+++ tests/0014/run-test.sh
@@ -0,0 +1,45 @@
+#! /bin/sh
+
+. ../test-utils.sh
+
+if [ -f testdb ]; then rm -f testdb; fi
+
+${AUDIODB} -d testdb -N
+
+intstring 2 > testfeature
+floatstring 0 1 >> testfeature
+floatstring 1 0 >> testfeature
+
+${AUDIODB} -d testdb -I -f testfeature
+
+echo "query point (0.0,0.5)"
+intstring 2 > testquery
+floatstring 0 0.5 >> testquery
+
+start_server ${AUDIODB} 10014
+
+${AUDIODB} -c localhost:10014 -d testdb -Q point -f testquery > testoutput
+echo testfeature 0.5 0 0 > test-expected-output
+echo testfeature 0 0 1 >> test-expected-output
+cmp testoutput test-expected-output
+${AUDIODB} -c localhost:10014 -d testdb -Q point -f testquery -n 1 > testoutput
+echo testfeature 0.5 0 0 > test-expected-output
+cmp testoutput test-expected-output
+
+check_server $!
+
+echo "query point (0.5,0.0)"
+intstring 2 > testquery
+floatstring 0.5 0 >> testquery
+
+${AUDIODB} -c localhost:10014 -d testdb -Q point -f testquery > testoutput
+echo testfeature 0.5 0 1 > test-expected-output
+echo testfeature 0 0 0 >> test-expected-output
+cmp testoutput test-expected-output
+${AUDIODB} -c localhost:10014 -d testdb -Q point -f testquery -n 1 > testoutput
+echo testfeature 0.5 0 1 > test-expected-output
+cmp testoutput test-expected-output
+
+stop_server $!
+
+exit 104
