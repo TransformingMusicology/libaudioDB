12051a228bb710cf1997e23f0097a4c90e883b15
mas01mc <mas01mc@7f337319-b335-0410-8187-a933a6394831>
2008-07-17 14:43:42 +0000

    abstracted RADII as list at top of script
    git-svn-id: svn+ssh://rvw/isms/group/omras2/src/svn/audioDB/trunk@448 7f337319-b335-0410-8187-a933a6394831

--- tests/pointset_test/run-test.sh
+++ tests/pointset_test/run-test.sh
@@ -1,68 +1,80 @@
 #! /bin/bash
 
 . ../test-utils.sh
-
-if [ -f testdb ]; then rm -f testdb; fi
-
-${AUDIODB} -d testdb -N
-
-intstring 1 > testpower
-floatstring -1 >> testpower
-
-${AUDIODB} -d testdb -P
-
 NPOINTS=100
-NDIM=10
+NDIM=100
+RADII="0.1 0.2 0.4 0.5 0.7 0.9 01 02 05 10"
+LSH_W="01 04"
 
-if [ -d rad.[0-9]* ]; then rm -r rad.[0-9]*; fi
-
-for j in .01 .02 .03 .05 01 02 03 05 09 10
-do
-  R_SQ=`echo "scale=6; $j^2" | bc`
-  mkdir -p "rad$j"
-  ./genpoints2 ${NPOINTS} ${R_SQ} ${NDIM}
-  mv testfeature* "rad$j"
-done
-
-for i in rad*[0-9]*/*
-do
-${AUDIODB} -d testdb -I -f $i -w testpower
-done
-
-# sequence queries require L2NORM
-${AUDIODB} -d testdb -L
-
-rm -f testdb.lsh.*
-
-LSH_W=1
-LSH_K=1
-LSH_M=1
-LSH_N=1000
-
-INDEXING=true
-if [ ${INDEXING} ]
+GENERATE_POINTS_AND_INSERT_IN_NEW_DB=true
+if [ ${GENERATE_POINTS_AND_INSERT_IN_NEW_DB} ]
     then
-    for j in .01 .02 .03 .05 01 02 03 05 09 10
+    if [ -f testdb ]; then rm -f testdb; fi
+    
+    ${AUDIODB} -d testdb -N
+    
+    intstring 1 > testpower
+    floatstring -1 >> testpower
+    
+    ${AUDIODB} -d testdb -P
+    
+    if [ -d rad*[0-9]* ]; then rm -r rad*[0-9]*; fi
+    
+    for j in ${RADII}
       do
       R_SQ=`echo "scale=6; $j^2" | bc`
-      ${AUDIODB} -d testdb -X -R ${R_SQ} -l 1 --lsh_N ${LSH_N} \
-	  --lsh_b ${LSH_N} --lsh_k ${LSH_K} --lsh_m ${LSH_M} --lsh_w ${LSH_W} \
-	  --absolute-threshold -1 --no_unit_norming
+      mkdir -p "rad$j"
+      ./genpoints2 ${NPOINTS} ${R_SQ} ${NDIM}
+      mv testfeature* "rad$j"
     done
+    
+    for i in rad*[0-9]*/*
+      do
+      ${AUDIODB} -d testdb -I -f $i -w testpower
+    done
+    
+# sequence queries require L2NORM
+    ${AUDIODB} -d testdb -L
 fi
 
-#if [ -f cumulativeResult.txt ]; then rm -f cumulativeResult.txt;fi
-
-for j in .01 .02 .03 .05 01 02 03 05 09 10
+for W in ${LSH_W}
   do
-  R_SQ=`echo "scale=6; $j^2" | bc`
-  ${AUDIODB} \
-      -d testdb -Q sequence -R ${R_SQ} -l 1 -e \
-      -f queryfeature -w testpower --absolute-threshold -1 --no_unit_norming -r 1000 > output${j}
-  echo `for k in .01 .02 .03 .05 01 02 03 05 09 10;do egrep "^rad$k" output${j} | wc | awk '{print $1}';done` >> cumulativeResult.txt
+  for LOOP1 in 1 2 3 4 5 6 7 8 9 10
+  do
+    for LOOP2 in 1 2 3 4 5
+      do
+      rm -f testdb.lsh.*
+      
+#  LSH_W=1
+      LSH_K=1
+      LSH_M=1
+      LSH_N=1000
+      
+      INDEXING=true
+      if [ ${INDEXING} ]
+	  then
+	  for j in ${RADII}
+	    do
+	    R_SQ=`echo "scale=6; $j^2" | bc`
+	    ${AUDIODB} -d testdb -X -R ${R_SQ} -l 1 --lsh_N ${LSH_N} \
+		--lsh_b ${LSH_N} --lsh_k ${LSH_K} --lsh_m ${LSH_M} --lsh_w ${W} \
+		--absolute-threshold -1 --no_unit_norming
+	  done
+      fi
+  
+#if [ -f cumulativeResult.txt ]; then rm -f cumulativeResult.txt;fi
+  
+      for j in ${RADII}
+	do
+	R_SQ=`echo "scale=6; $j^2" | bc`
+	${AUDIODB} \
+	    -d testdb -Q sequence -R ${R_SQ} -l 1 -e \
+	    -f queryfeature -w testpower --absolute-threshold -1 --no_unit_norming -r 1000 > output${j}
+	echo `for k in ${RADII};do egrep "^rad$k" output${j} | wc | awk '{print $1}';done` >> cumulativeResult_k1_w${W}_m1_${NDIM}.txt
+      done
+    done
+  done
 done
-
-
 
 #Perform exact search as a sanity test
 #rm -f *.lsh*
