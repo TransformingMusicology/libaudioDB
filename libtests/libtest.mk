31523c783ad0440f1f6a26a1a8277f9e761a62f3
Ben Fields <me@benfields.net>
2008-11-14 15:50:09 +0000

    libtests are now compatible with the dylib created on mac osx
    git-svn-id: svn+ssh://rvw/isms/group/omras2/src/svn/audioDB/trunk@648 7f337319-b335-0410-8187-a933a6394831

--- libtests/libtest.mk
+++ libtests/libtest.mk
@@ -1,13 +1,34 @@
-all: libaudioDB.so.0 libaudioDB.so test1
+EXECUTABLE=audioDB
+SOVERSION=0
+MINORVERSION=0
+LIBRARY_FULL=lib$(EXECUTABLE).so.$(SOVERSION).$(MINORVERSION)
+LIBRARY_VERS=lib$(EXECUTABLE).so.$(SOVERSION)
+LIBRARY=lib$(EXECUTABLE).so
+ARCH_FLAGS=
 
-libaudioDB.so.0:
-	-ln -s ../../libaudioDB.so.0.0 $@
 
-libaudioDB.so: libaudioDB.so.0
+ifeq ($(shell uname),Darwin)
+override LIBRARY_FULL=lib$(EXECUTABLE).$(SOVERSION).$(MINORVERSION).dylib
+override LIBRARY_VERS=lib$(EXECUTABLE).$(SOVERSION).dylib
+override LIBRARY=lib$(EXECUTABLE).dylib
+ifeq ($(shell sysctl -n hw.optional.x86_64),1)
+override ARCH_FLAGS+=-arch x86_64
+endif
+endif
+
+all: $(LIBRARY_FULL) $(LIBRARY_VERS) $(LIBRARY) test1
+
+$(LIBRARY_FULL):
+	-ln -s ../../$(LIBRARY_FULL) $@
+
+$(LIBRARY_VERS):
+	-ln -s ../../$(LIBRARY_FULL) $@
+
+$(LIBRARY): $(LIBRARY_VERS)
 	-ln -s $< $@
 
 test1: prog1.c
-	gcc -Wall -laudioDB -L. -Wl,-rpath,. -o $@ $<
+	gcc -Wall $(ARCH_FLAGS) -laudioDB -L. -Wl,-rpath,. -o $@ $<
 
 clean:
-	-rm libaudioDB.so.0 libaudioDB.so
+	-rm $(LIBRARY_FULL) $(LIBRARY_VERS) $(LIBRARY)
