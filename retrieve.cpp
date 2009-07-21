4306c9d4a2cf5f5b98853208a8ced0c7982771bc
Christophe Rhodes <csr21@cantab.net>
2009-07-21 15:57:24 +0000

    Add retrieve.cpp, missing from previous commit.
    It's been a while since I've done that.
    
    git-svn-id: svn+ssh://rvw/isms/group/omras2/src/svn/audioDB/trunk@882 7f337319-b335-0410-8187-a933a6394831

---
+++ retrieve.cpp
@@ -0,0 +1,17 @@
+extern "C" {
+#include "audioDB_API.h"
+}
+#include "audioDB-internals.h"
+
+int audiodb_retrieve_datum(adb_t *adb, const char *key, adb_datum_t *datum) {
+  uint32_t index = audiodb_key_index(adb, key);
+  if(index == (uint32_t) -1) {
+    return 1;
+  } else {
+    return audiodb_track_id_datum(adb, index, datum);
+  }
+}
+
+int audiodb_free_datum(adb_t *adb, adb_datum_t *datum) {
+  return audiodb_really_free_datum(datum);
+}
